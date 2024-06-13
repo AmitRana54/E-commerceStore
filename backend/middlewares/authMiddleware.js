303f8d1523a703fa0218fcba98810483fba49163
AmitRana54 <109697933+AmitRana54@users.noreply.github.com>
2024-06-14 00:54:21 +0530

    productadd

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -7,6 +7,7 @@ const authenticate = asyncHandler(async (req, res, next) => {
 
   // Read JWT from the 'jwt' cookie
   token = req.cookies.jwt;
+  console.log(token);
 
   if (token) {
     try {
@@ -24,10 +25,11 @@ const authenticate = asyncHandler(async (req, res, next) => {
 });
 
 const authorizeAdmin = (req, res, next) => {
+  console.log(req.user);
   if (req.user && req.user.isAdmin) {
     next();
   } else {
-    res.status(401).send("Not authorized as an admin.");
+    res.status(401).send("user is not authorized as admin");
   }
 };
 
